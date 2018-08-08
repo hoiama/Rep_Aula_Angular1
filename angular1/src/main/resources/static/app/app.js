adfd3e564fbf815f90070015ca7e4ad61425d667
hoiama <hoiama9@gmail.com>
2018-08-08 19:17:32 -0300

    Separando projeto do caronas

--- angular1/src/main/resources/static/app/app.js
+++ angular1/src/main/resources/static/app/app.js
@@ -1,6 +1,9 @@
 
 /**
  * Declaração de módulo 'app' usando modulo ngRoute para criacao de rotas
+ *
+ * ngRoute -  sistema de roteamento de URLs
+ * angular.filter - filtro personalizado criado
  */
 var app = angular.module('app', ['ngRoute', 'ngResource', 'angular.filter']);
 
@@ -15,27 +18,18 @@ app.config([
                 controller:'DefaultController',
                 templateUrl:'templates/default-template.html'
             })
-            .when('/minhas/caronas', {
-                controller:'MinhasCaronasController',
-                templateUrl:'templates/minhas-caronas-template.html'
-            })
-            .when('/buscar/caronas', {
-                controller:'BuscarController',
-                templateUrl:'templates/buscar-caronas-template.html'
-            })
             .when('/testesAngular', {
                 controller:'TestesAngularController',
-                templateUrl:'templates/testes/testes-angular-controller.html',
-            })
-            .when('/templateUrlReplace', {
-                templateUrl:'templates/testes/fragmentoHtml.html',
-                replace:true
+                templateUrl:'templates/controller.html',
             })
             .otherwise({redirectTo:'/'});
     }
 
 ]);
 
+app.directive("minhaDiretiva", minhaDiretiva);
+app.directive("meuPainel", meuPainel);
+app.directive("minhaDiretiva", minhaDiretiva);
 
 /**
  * Variavel de escopo global da aplicação
@@ -49,6 +43,7 @@ app.run([
     }
 ]);
 
+// ** CONTROLLER's **
 
 /**
  * Controller padrão da página princinal
@@ -58,92 +53,7 @@ app.controller('DefaultController', function(){
 })
 
 
-/**
- * Controller que edita elementos da lista de frutas
- * @param {scopo} $scope - Escopo que pode ser usada no controller criado
- * @param {location} $location - Redireciona as rotas
- * @param {routeParams} $routeParams - Obtem parametros repassados pela URI
- * Adiciona um novo item a lista de frutas
- * @param {scopo} $scope - Escopo que pode ser usada no controller criado
- * @param {} $location - Redireciona as rotas
- * @param {tipo} $routeParams - Obtem parametros repassados pela URI  
- */
-app.controller('MinhasCaronasController', ['$scope',
-                                            '$location',
-                                            '$routeParams',
-                                            '$http', function($scope, $location, $routeParams, $http){
-
-    $scope.listaRotas =  Array();
-    $scope.save = function(partida, chegada){
-        var rota = {
-            chegada: $scope.chegada,
-            partida: $scope.chegada
-        }
-        $scope.listaRotas.push(rota);
-    }
-
-    $scope.postData = function(){
-        $http
-            .post()
-            .then();
-    };
-}]);
-
-
-app.factory('AnalisaListaRotasRepeditas', ['$window', function (win) {
-
-}]);
-
-
-app.controller('BuscarController', ['$scope', '$http',function($scope, $http){
-
-    $scope.listCaronas = Array();
-
-    $scope.getData = function(){
-        $http
-            .get('/getCarona')
-            .then(
-                function(respose){
-                    $scope.listCaronas = respose.data;
-                },
-                function(erro){
-                    alert('Erro..."');
-                    console.log(erro);  
-                }
-            )      
-    }
-
-
-    $scope.getData2 = function(){
-        $http.get('/getCarona')
-            .success(function(retorno) {
-                console.log(retorno);
-                $scope.listCaronas = retorno;
-            })
-            .error(function(erro) {
-                console.log(erro);
-            });
-    }
-
-
-     $scope.fotos = [];
-
-        var promise = $http.get('/getCarona');
-        promise.then(function(retorno) {
-            $scope.listCaronas = retorno.data;;
-        })
-        .catch(function(erro) {
-            console.log(erro)
-        });
-}])
-
-
-
-/**
- * TESTES ANGULAR1
- */
-
-app.controller("TestesAngularController", ['$scope', '$filter', function($scope, $filter){
+app.controller("TestesAngularController", ['$scope', '$filter', '$http', 'fabricaHttpPromise', function($scope, $filter, $http, fabricaHttpPromise){
     $scope.colors = ["White", "Black", "Blue", "Red", "Silver"];
     $scope.colors2 = ["Branco", "Preto", "Azul", "Vermelho", "Cinza"];
     $scope.mostraEsconde = ["ng-hide", "ng-if", "ng-show"];
@@ -161,6 +71,7 @@ app.controller("TestesAngularController", ['$scope', '$filter', function($scope,
     $scope.placa = "";
     $scope.cpf = "";
     $scope.cpf2 = "";
+    $scope.listCaronas = Array();
 
     $scope.carros =
         [
@@ -168,28 +79,43 @@ app.controller("TestesAngularController", ['$scope', '$filter', function($scope,
             {vidro: 1, portas: 4,mala: 7},
             {vidro: 3, portas: 6,mala: 9},
         ]
+
+
+    $scope.getListCaronas = function(){
+        fabricaHttpPromise.getCaronas
+            .then(function(data, status) {
+                console.log("retorno: ", data);
+                console.log("estado: " , status);
+                $scope.listCaronas = data.data;
+            })
+            .catch(function(erro) {
+                console.log(erro)
+            });
+    }
 }]);
 
 
-app.directive("my-template", function(){
+// ** DIRETIVAS **
+
+/**
+ * Demostração de templates
+ */
+function minhaDiretiva(){
     return {
         template:
             "<div class='alert'>" +
                 "<span class='alert-topic'>" +
-                    "Something went wrong!" +
-                "</span>" +
-                "<span class='alert-description'>" +
-                    "You must inform the plate and the color of the car!" +
+                    "Sou um template da diretiva minhaDiretiva!" +
                 "</span>" +
             "</div>"
     };
-});
+};
 
 
 /**
- * Diretiva que retorna um ddo contendo template e propriedades do scope.
+ * Diretiva que retorna um ddo contendo template e propriedades do scope. parte 13
  */
-app.directive("meuPainel", function(){
+ function meuPainel(){
     restrict: 'AE';
     var ddo = {};
 
@@ -199,13 +125,13 @@ app.directive("meuPainel", function(){
 
     ddo.template = '<p>Sou um Template da diretiva meuPainel, minha propriedade eh: {{propriedade1}} </p>';
     return ddo;
-});
+};
 
 
 /**
  * Diretiva que retorna um ddo contendo template e propriedades do scope.
  */
-app.directive("minhaDiretiva", function(){
+function minhaDiretiva (){
     restrict:"AE";
     var ddo = {};
 
@@ -213,10 +139,12 @@ app.directive("minhaDiretiva", function(){
         propriedade1: '@propriedade1'
     }
 
-    ddo.templateUrl = "templates/testes/fragmentoHtml.html"
+    ddo.templateUrl = "templates/fragmentoHtml.html"
     return ddo;
-});
+};
 
+
+// ** FILTROS **
 
 app.filter("filtroPlacas", function(){
     return function (input) {
@@ -229,8 +157,30 @@ app.filter("filtroPlacas", function(){
 });
 
 
-/**
- * Estudar:
- * testes de controller https://docs.angularjs.org/guide/controller
- * Layouts : https://material.angularjs.org/1.1.5/layout/introduction
- */
+// ** FABRICAS **
+
+app.factory("fabricaHttpPromise", function ($http) {
+    var _getCaronas = function () {
+        return $http.get("/carona");
+    }
+
+    var _postCarona = function (carona) {
+        return $http.post("/carona", carona);
+    }
+
+    var _putCarona = function (carona) {
+        return $http.put("/carona", carona);
+    }
+
+    var _deleteCarona = function(carona){
+        return $http.delete("/carona");
+    }
+
+    return {
+        getCaronas : _getCaronas(),
+        postCarona : _postCarona(),
+        putCarona : _putCarona(),
+        deleteCarona : _deleteCarona()
+    }
+})
+
